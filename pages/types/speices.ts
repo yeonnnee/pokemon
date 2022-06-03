8f01964831a8e6f11b5f6aa6b1b5ed2fee3a91b8
yeonnnee <yeonnnee@gmail.com>
2022-06-03 12:20:33 +0900

    species 타입 정의

---
+++ pages/types/speices.ts
@@ -0,0 +1,67 @@
+import { ResourceForPokemon } from "./pokemons"
+
+
+export interface PokemonSpeciesApiRes {
+  base_happiness: number, // v
+  capture_rate: number, // v 
+  color: ResourceForPokemon,
+  egg_groups: ResourceForPokemon[],
+  evolution_chain: {
+    url: string
+  },
+  evolves_from_species: null,
+  flavor_text_entries: FlavorTextEntry[],
+  form_descriptions: any[],
+  forms_switchable: boolean,
+  gender_rate: number,
+  genera: Genera[], // v
+  generation: ResourceForPokemon,
+  growth_rate: ResourceForPokemon,
+  habitat: ResourceForPokemon,
+  has_gender_differences: boolean,
+  hatch_counter: number,
+  id: number,
+  is_baby: boolean,
+  is_legendary: boolean,
+  is_mythical: boolean,
+  name: string,
+  names: PokemonName[], //v
+  order: number,
+  pal_park_encounters: PalParkEncounter[],
+  pokedex_numbers: PokedexNumber[],
+  shape: ResourceForPokemon,
+  varieties: Varity[]
+}
+
+export interface PokemonName {
+  language: ResourceForPokemon,
+  name: string // 이상해씨
+}
+
+export interface PalParkEncounter {
+  area: ResourceForPokemon,
+  base_score: number,
+  rate: number
+}
+
+export interface Varity {
+  is_default: boolean,
+  pokemon: ResourceForPokemon
+}
+
+export interface PokedexNumber {
+  entry_number: number,
+  pokedex: ResourceForPokemon
+}
+
+export interface FlavorTextEntry {
+  flavor_text: string,
+  language: ResourceForPokemon,
+  version: ResourceForPokemon
+}
+
+export interface Genera {
+  genus: string, //  씨앗포켓몬
+  language: ResourceForPokemon
+}
+
