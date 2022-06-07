f5bd0faf19553da10ebf8e608f4b6b81d85df843
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-07 17:24:21 +0900

    ref 배열로 관리

--- pages/pokemon/[pokemonName]/index.tsx
+++ pages/pokemon/[pokemonName]/index.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import detailStyle from '../../../styles/detail.module.scss';
-import { PokemonDetail, PokemonDetailApiRes, PokemonStat } from "../../types/detail";
-import { PokemonSpeciesApiRes } from "../../types/speices";
+import { PokemonDetail, PokemonDetailApiRes, PokemonStat } from "../../../types/detail";
+import { PokemonSpeciesApiRes } from "../../../types/speices";
+import { AbilityApiRes } from "../../../types/ability";
+import ImageCard from "./ImageCard";
+import usePokemonIdx from "../../../hooks/usePokemonIdx";
 
 
 const Detail = () => {
@@ -12,63 +15,18 @@ const Detail = () => {
   const [data, setData] = useState<PokemonDetail | null>(null);
   const [selectedVersion, setSelectedVersion] = useState<number>(0);
   const [loading, setLoading] = useState(true);
+  const pokemonIdx = usePokemonIdx(data?.order || 0);
 
-  const happinessBarRef = useRef<HTMLDivElement | null>(null);
-  const hpBarRef = useRef<HTMLDivElement | null>(null);
-  const attackBarRef = useRef<HTMLDivElement | null>(null);
-  const defenseBarRef = useRef<HTMLDivElement | null>(null);
-  const spAttackBarRef = useRef<HTMLDivElement | null>(null);
-  const spDefenseBarRef = useRef<HTMLDivElement | null>(null);
-  const speedBarRef = useRef<HTMLDivElement | null>(null);
-
-
-
-  function getThreeDigitsIdx(pokemonOrder: number) {
-    if(pokemonOrder < 10) {
-      return `00${pokemonOrder}`;
-    } else if(pokemonOrder > 9) {
-      return `0${pokemonOrder}`;
-    } else {
-      return pokemonOrder.toString();
-    }
-  }
+  const barRef = useRef<HTMLDivElement[] | null[]>([]);
 
   const paintGraphBar = useCallback((result:PokemonDetail | null) => {
     if (!result || loading) return;
-    console.log('paint', result)
-    const happines = result.happiness;
-    const hpRate = result.stats.find(stat => stat.stat.name === 'hp')?.base_stat;
-    const attackRate = result.stats.find(stat => stat.stat.name === 'attack')?.base_stat;
-    const defenseRate = result.stats.find(stat => stat.stat.name === 'defense')?.base_stat;
-    const spAttackRate = result.stats.find(stat => stat.stat.name === 'special-attack')?.base_stat;
-    const spDefenseRate = result.stats.find(stat => stat.stat.name === 'special-defense')?.base_stat;
-    const speedRate = result.stats.find(stat => stat.stat.name === 'speed')?.base_stat;
+    const stats = result.stats.map(stat => stat.base_stat);
 
-    console.log(happinessBarRef)
-
-    if (!happinessBarRef.current) return;
-    happinessBarRef.current.style.width = `${ (happines && happines > 100) ? 100 : happines}%`;
-
-    console.log(happinessBarRef)
-    if (!spAttackBarRef.current) return;
-    spAttackBarRef.current.style.width = `${(spAttackRate && spAttackRate > 100) ? 100 : spAttackRate}%`;
-    console.log(2)
-    
-    if (!spDefenseBarRef.current) return;
-    spDefenseBarRef.current.style.width = `${(spDefenseRate && spDefenseRate > 100) ? 100 : spDefenseRate}%`;
-    console.log(3)
-
-    if (!hpBarRef.current) return;
-    hpBarRef.current.style.width = `${(hpRate && hpRate > 100) ? 100 : hpRate}%`;
-
-    if (!attackBarRef.current) return;
-    attackBarRef.current.style.width = `${(attackRate && attackRate > 100) ? 100 : attackRate}%`;
-
-    if (!defenseBarRef.current) return;
-    defenseBarRef.current.style.width = `${(defenseRate && defenseRate > 100) ? 100 : defenseRate}%`;
-
-    if (!speedBarRef.current) return;
-    speedBarRef.current.style.width = `${(speedRate && speedRate > 100) ? 100 : speedRate}%`;
+    barRef.current.forEach((ref, index) => {
+      if (!ref) return;
+      ref.style.width = `${(stats[index] && stats[index] > 100) ? 100 : stats[index]}%`
+    });
   }, [loading]);
 
   const convertStatName = (name: string) => {
@@ -83,18 +41,6 @@ const Detail = () => {
     }
   }
 
-  const getStatRef = (name: string) => {
-    switch (name) { 
-      case 'hp' : return hpBarRef;
-      case 'attack': return attackBarRef;
-      case 'defense': return defenseBarRef;
-      case 'special-attack': return spAttackBarRef;
-      case 'special-defense': return spDefenseBarRef;
-      case 'speed': return speedBarRef;
-      default: return happinessBarRef;
-    }
-  }
-
   const getDetailData = useCallback(async() => {
     if(!pokemonName) return;
     const detailRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
@@ -105,17 +51,32 @@ const Detail = () => {
 
     const nameKr = species.names.filter(name => name.language.name === 'ko')[0];
     const desc = species.flavor_text_entries.filter(text => text.language.name === 'ko');
-    console.log(desc)
+
+    const abilitiesKr = await Promise.all(detail.abilities.map(async (ability) => {
+      const abilityDetail: AbilityApiRes = await fetch(ability.ability.url).then(data => data.json());
+      const result = abilityDetail.flavor_text_entries.filter(effect => effect.language.name === 'ko');
+      return {
+        text: result,
+        name: abilityDetail.names.filter(name => name.language.name === 'ko')[0]
+      };
+    }));
+      
+    console.log('detail', detail);
+    console.log('species', species);
+    console.log('abilitiesKr', abilitiesKr);
+
     const result = {
       name: species.name,
       nameKr: nameKr.name, 
       names: species.names,
       desc: desc,
-      order: getThreeDigitsIdx(detail.order),
+      order: detail.order,
       weight: detail.weight,
       height: detail.height,
       types: detail.types,
       images: detail.sprites,
+      // abilities: detail.abilities,
+      abilitiesKr: abilitiesKr,
       happiness: species.base_happiness,
       capture_rate: species.capture_rate,
       growth_rate: species.growth_rate,
@@ -128,13 +89,12 @@ const Detail = () => {
         return {
           ...stat,
           label: convertStatName(stat.stat.name),
-          ref: getStatRef(stat.stat.name),
         }
       })
     };
 
-    result.stats.push({url:'', base_stat: result.happiness, stat:{name: 'happiness', url:''}, label: 'Happiness', ref: getStatRef('happiness'),})
-    console.log(result);
+    result.stats.push({url:'', base_stat: result.happiness, stat:{name: 'happiness', url:''}, label: 'Happiness'})
+
     setLoading(false);
     setData(result);
     paintGraphBar(result);
@@ -154,43 +114,23 @@ const Detail = () => {
           data ? <Image priority width={400} height={400} src={data.images.other["official-artwork"].front_default || ''} alt={data.name}/> : <span>No Image</span>
         }
         <div className={detailStyle.pic}>
-          <div>
-            {
-              data ? <Image priority width={100} height={100} src={data.images.back_default || ''} alt={data.name}/> : <span>No Image</span>
-            }
-          </div>
-          <div>
-            {
-              data ? <Image priority width={100} height={100} src={data.images.back_shiny || ''} alt={data.name}/> : <span>No Image</span>
-            }
-          </div>
-          <div>
-            {
-              data ? <Image priority width={100} height={100} src={data.images.front_default || ''} alt={data.name}/> : <span>No Image</span>
-            }
-          </div>
-          <div>
-            {
-              data ? <Image priority width={100} height={100} src={data.images.front_shiny || ''} alt={data.name}/> : <span>No Image</span>
-            }
-          </div>
+        {/* <ImageCard width={100} height={100} src={data?.images.back_shiny} alt={data?.name}/>
+          <ImageCard width={100} height={100} src={data?.images.back_default} alt={data?.name}/> */}
+          <ImageCard width={100} height={100} src={data?.images.front_default} alt={data?.name}/>
+          <ImageCard width={100} height={100} src={data?.images.front_shiny} alt={data?.name}/>
+
+          <ImageCard width={100} height={100} src={data?.images.other.home.front_default} alt={data?.name}/>
+          <ImageCard width={100} height={100} src={data?.images.other.home.front_shiny} alt={data?.name} />
+
         </div>
 
       </section>
 
       <section className={detailStyle["pokemon-info-section"]}>
-        <span className={detailStyle.order}>No.{data?.order}</span>
+        <span className={detailStyle.order}>No.{pokemonIdx}</span>
         <div className={detailStyle.name}>
           <span>{data?.nameKr}</span>
         </div>
-
-        {
-              data ? <Image width={100} height={100} src={data.images.other.home.front_default|| ''} alt={data.name}/> : <span>No Image</span>
-        }
-        
-        {
-              data ? <Image width={100} height={100} src={data.images.other.home.front_shiny|| ''} alt={data.name}/> : <span>No Image</span>
-            }
 
         {/* 특징 */}
         <div className={`${detailStyle.desc} ${detailStyle.section}`}>
@@ -211,7 +151,7 @@ const Detail = () => {
               <div className={detailStyle['graph-section']} key={index}>
                 <p>{ stat.label }</p>
                 <div className={ detailStyle.graph }>
-                  <div ref={stat.ref} className={`${detailStyle['graph-bar']} ${detailStyle[`${stat.stat.name}-bar`]}`}></div>
+                  <div ref={el => (barRef.current[index] = el)} className={`${detailStyle['graph-bar']} ${detailStyle[`${stat.stat.name}-bar`]}`}></div>
                 </div>
                 <p>{ stat.base_stat }</p>
               </div>
@@ -222,7 +162,7 @@ const Detail = () => {
         {/* Info */}
         <ul className={`${detailStyle.info} ${detailStyle.section}`}>
           <li>
-            <p className={detailStyle.category}>Type</p>
+            <p className={detailStyle.category}>타입</p>
             <div className={detailStyle.type}>
             {
               data?.types.map((type, index) => {
@@ -233,27 +173,33 @@ const Detail = () => {
 
           </li>
 
-
           <li>
-            <p className={detailStyle.category}>Height</p>
+            <p className={detailStyle.category}>신장</p>
             <p>{data?.height}m</p>
           </li>
           
           
           <li>
-            <p className={detailStyle.category}>Weight</p>
+            <p className={detailStyle.category}>체중</p>
             <p>{data?.weight} kg</p>
           </li>
 
           <li>
-            <p className={detailStyle.category}>Generation</p>
+            <p className={detailStyle.category}>세대</p>
             <p>{data?.generation.name}</p>
           </li>
 
           
           <li>
-            <p className={detailStyle.category}>Category</p>
+            <p className={detailStyle.category}>분류</p>
             <p>{data?.genera[0].genus}</p>
+          </li>
+
+          <li>
+            <p className={detailStyle.category}>특성</p>
+            {data?.abilitiesKr.map((ability, index) => {
+              return (<p key={index}>{ ability.name.name}</p>)
+            })}
           </li>
         </ul>
       </section>
