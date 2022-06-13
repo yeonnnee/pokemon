4075f23dbce53086bd6ca2f09a932cc15d5c2ea2
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-13 20:25:23 +0900

    type ap res 타입 추가

--- types/pokemonTypes.ts
+++ types/pokemonTypes.ts
@@ -1,4 +1,5 @@
 import { ResourceForPokemon } from "./pokemons";
+import { PokemonName } from "./speices";
 
 export interface PokemonTypesApiRes {
   count: number,
@@ -11,4 +12,34 @@ export interface CustomPokemonType {
   name: string,
   nameKr: string,
   url: string
+}
+
+export interface TypeDetailApiRes {
+  damage_relations: Damage,
+  game_indices: Game[],
+  generation: ResourceForPokemon,
+  id: number,
+  move_damage_class: ResourceForPokemon,
+  moves: ResourceForPokemon[],
+  name: string,
+  names: PokemonName[],
+  past_damage_relations: {
+    damage_relations: Damage,
+    generation: ResourceForPokemon,
+  },
+  pokemon: {pokemon: ResourceForPokemon, slot: number} []
+}
+
+export interface Damage {
+  double_damage_from: ResourceForPokemon[],
+  double_damage_to: ResourceForPokemon[],
+  half_damage_from: ResourceForPokemon[],
+  half_damage_to: ResourceForPokemon[],
+  no_damage_from: ResourceForPokemon[],
+  no_damage_to: ResourceForPokemon[],
+}
+
+export interface Game {
+  game_index: number,
+  generation: ResourceForPokemon
 }
