4e8503ea61fbc151f0438199cad92ede936959ae
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-19 23:49:09 +0900

    필요한 번역 텍스트 생성

---
+++ translate/text.js
@@ -0,0 +1,62 @@
+export const title = [
+  {
+    "language": "ko",
+    "text": "포켓몬"
+  },
+  {
+    "language": "en",
+    "text": "Pokémon"
+  },
+  {
+    "language": "ja",
+    "text": "ポケモン"
+  }
+];
+
+export const placeholder = [
+  {
+    "language": "ko",
+    "text": "포켓몬 이름을 입력해주세요"
+  },
+  {
+    "language": "en",
+    "text": "Please enter Pokemon name"
+  },
+  {
+    "language": "ja",
+    "text": "ポケモン名を入力してください"
+  }
+];
+
+export const categoryName = [
+  {
+    "language": "ko",
+    "text": "타입",
+    "category":"type"
+  },
+  {
+    "language": "en",
+    "text": "Type",
+    "category":"type"
+  },
+  {
+    "language": "ja",
+    "text": "タイプ",
+    "category":"type"
+  }
+];
+
+export const reset = [
+  {
+    "language": "ko",
+    "text": "초기화",
+  },
+  {
+    "language": "en",
+    "text": "Reset",
+  },
+  {
+    "language": "ja",
+    "text": "リセット",
+  }
+]
