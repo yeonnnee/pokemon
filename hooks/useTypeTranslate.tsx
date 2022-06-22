e67daf4137162a0ca6277abc4a66b22be63591ad
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-22 21:57:14 +0900

    언어에 따라 타입이름 return 하는 hook 생성

---
+++ hooks/useTypeTranslate.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useState } from "react";
+import { PokemonType } from "../types/detail";
+import { TypeDetailApiRes } from "../types/pokemonTypes";
+
+function useTypeTranslate(types: PokemonType[], lang) {
+  const [supportLang, setSupportLang] = useState('');
+  const [translatedTypes, setTranslatedTypes] = useState<string[]>([]);
+  
+
+  const convertTypeName = useCallback(async () => {
+    const typeNm = await Promise.all(types.map(async (type) => {
+      const res:TypeDetailApiRes = await fetch(type.type.url).then(res => res.json());
+      const name = res.names.filter(name => name.language.name === lang)[0].name;
+      return name;
+    }));
+    setTranslatedTypes(typeNm);
+  }, [types, lang]);
+
+  useEffect(() => {
+    if (supportLang === lang) return;
+    setSupportLang(lang);
+    convertTypeName();
+
+  }, [supportLang, lang, convertTypeName ]);
+  return translatedTypes;
+}
+
+export default useTypeTranslate;
