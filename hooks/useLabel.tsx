431b150f58e7bfb575afa5e019c21c2c9fe11146
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-23 20:17:40 +0900

    gmaxText 수정

--- hooks/useLabel.tsx
+++ hooks/useLabel.tsx
@@ -5,7 +5,7 @@ function useLabel(pokemonName: string, lang: string, pokemonIdx: string) {
   const [label, setLabel] = useState('');
   
   useEffect(() => {
-    const gmaxText = gmaxLabel.filter(text => text.language == lang)[0].text;
+    const gmaxText = gmaxLabel.filter(text => text.language == lang)[0]?.text;
     const label = pokemonName.includes('gmax') ? `${gmaxText}` : `No.${pokemonIdx}`;
     setLabel(label);
 
