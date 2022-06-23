85d603b11b0e6b86701485ac6608cf3ee30b3456
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-23 18:29:21 +0900

    title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import Nav from '../components/common/Nav';
+
 import Head from 'next/head';
+import Nav from '../components/common/Nav';
 
 config.autoAddCss = false;
 
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Pokedex</title>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <Nav />
